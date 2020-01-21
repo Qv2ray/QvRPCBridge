e86ee03d02dbbbcaca10471e0079f6a88cd1bed5
DuckSoft <realducksoft@gmail.com>
2020-01-21 16:13:58 +0800

    changing to build archive for static linking

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir build
 cd build || exit 233
-go build -buildmode=c-shared -ldflags "-s -w" -o libqvb.so ../main.go
+go build -buildmode=c-archive -ldflags "-s -w" -o libqvb.a ../main.go
